5b42138833087f2c26bbc42892b30848e61bc5e3
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2024-10-26 03:00:58 +0700

    fix: updated fams data entries

--- modules/fams-data.ts
+++ modules/fams-data.ts
@@ -2,4 +2,6 @@ import parsed_data from './fams-data.json';
 
 export type FamsDataType = typeof parsed_data.data[0];
 
+export type Majors = "Ilmu Komputer Reguler" | "Sistem Informasi" | "Ilmu Komputer KKI";
+
 export const famsData = parsed_data.data
