65e593979b15fa821364dbcbf276150904e23fa3
kevincornellius <kevincornelius1905@gmail.com>
2024-10-26 11:05:15 +0700

    fix: bg

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -3,14 +3,20 @@ import LandingPageHero from "./sections/LandingPageHero";
 import OurMoments from "./sections/OurMoments";
 import SwitchingCard from "./sections/SwitchingCard";
 import Quotes from "./sections/Quotes";
+import CSStudy from "./sections/CSStudy";
+import CSCorner from "./sections/CSCorner";
 
 const LandingPage = () => {
   return (
     <div>
       <LandingPageHero />
-      <Quotes />
-      <SwitchingCard />
-      <OurMoments />
+      <div className="bgGrad">
+        <Quotes />
+        <SwitchingCard />
+        <OurMoments />
+        <CSCorner />
+        <CSStudy />
+      </div>
     </div>
   );
 };
