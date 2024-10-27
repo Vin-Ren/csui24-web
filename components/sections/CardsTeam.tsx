d984b7dc2ade33594a0b7e175f7b3b83c5a68078
Muhammad Fauzan <muhammadfauzan@Muhammads-MacBook-Air.local>
2024-10-27 16:59:08 +0700

    fix: page teams

--- components/sections/CardsTeam.tsx
+++ components/sections/CardsTeam.tsx
@@ -1,18 +1,10 @@
 'use client';
 import Image from "next/image";
 import React, { useState, useEffect } from "react";
+import Link from "next/link";
 
-const data = {
-    nickname: "LASKAR",
-    fontNickname: ["32px", "21px"],
-    fullname: "Amadio Juno",
-    Divisi: "Akademik",
-    fontDivisi: ["30px", "20px"],
-    image: "/team/tes.png",
-    image2: "/team/tes2.png"
-};
 
-const CardsTeam = () => {
+const CardsTeam = (props: any) => {
     const [isSmallScreen, setIsSmallScreen] = useState(false);
 
     useEffect(() => {
@@ -27,6 +19,8 @@ const CardsTeam = () => {
 
     return (
         <>
+            <Link href={props.link}>
+
             <div className="w-[186px] h-[237px] max-md:w-[134px] max-md:h-[171px] bg-[#292929] rounded-[14.2px] customShadow relative overflow-hidden z-0 group">
                 <Image
                     src="/elements/element1.svg"
@@ -43,30 +37,30 @@ const CardsTeam = () => {
                     className="transform rotate-[164.592deg] absolute bottom-2 left-1/2 -translate-x-1/2 z-[1] fill-custom-fill scale-150"
                 />
 
-                <div className="w-fit text-[#b3b3b3] text-center tracking-widest font-[400] font-UncialAntiqua flex flex-col gap-20 items-center z-[2] absolute top-[18px] left-1/2 -translate-x-1/2 opacity-70">
+                <div className="w-fit text-[#b3b3b3] text-center tracking-widest font-[400] font-UncialAntiqua flex flex-col gap-20 items-center z-[2] absolute top-[13px] left-1/2 -translate-x-1/2 opacity-70">
                     <p
                         className={`transition-opacity duration-500 group-hover:opacity-0 select-none ${isSmallScreen ? "leading-[30px]" : "leading-[37px]"}`}
-                        style={{ fontSize: isSmallScreen ? data.fontNickname[1] : data.fontNickname[0] }}
+                        style={{ fontSize: isSmallScreen ? props.fontNickname[1] : props.fontNickname[0] }}
                     >
-                        {data.nickname}<br />{data.nickname}<br />{data.nickname}<br />{data.nickname}
+                        {props.nickname}<br />{props.nickname}<br />{props.nickname}<br />{props.nickname}
                     </p>
                     <p
-                        className={`absolute top-0 transition-opacity duration-500 opacity-0 group-hover:opacity-100 select-none ${isSmallScreen ? "leading-[30px]" : "leading-[37px]"}`}
-                        style={{ fontSize: isSmallScreen ? data.fontDivisi[1] : data.fontDivisi[0] }}
+                        className={`w-max absolute top-[-5px] transition-opacity duration-500 opacity-0 group-hover:opacity-100 select-none ${isSmallScreen ? "leading-[30px]" : "leading-[37px]"}`}
+                        style={{ fontSize: isSmallScreen ? props.fontDivisi[1] : props.fontDivisi[0] }}
                     >
-                        {data.Divisi}<br />{data.Divisi}<br />{data.Divisi}<br />{data.Divisi}<br />
+                        {props.Divisi}<br />{props.Divisi}<br />{props.Divisi}<br />{props.Divisi}<br />
                     </p>
                 </div>
 
                 <Image
-                    src={data.image}
+                    src={props.image}
                     alt="image"
                     width={isSmallScreen ? 120 : 166}
                     height={isSmallScreen ? 142 : 198}
                     className="z-[3] absolute bottom-[15%] left-1/2 -translate-x-1/2 transition-opacity duration-500 group-hover:opacity-0"
                 />
                 <Image
-                    src={data.image2}
+                    src={props.image2}
                     alt="image"
                     width={isSmallScreen ? 120 : 166}
                     height={isSmallScreen ? 142 : 198}
@@ -75,7 +69,7 @@ const CardsTeam = () => {
 
                 <div className="z-[4] w-[110px] max-md:w-[80px] h-[23px] max-md:h-[16px] bg-[#1E1B4C] absolute bottom-[12%] flex flex-col items-center filter drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] opacity-95">
                     <p className="w-max text-[#D9D9D9] text-[12px] max-md:text-[8px] font-[400] font-PalanquinDark absolute left-1/2 -translate-x-1/2">
-                        {data.fullname}
+                        {props.fullname}
                     </p>
                     <Image
                         src="/elements/triangle.svg"
@@ -88,7 +82,7 @@ const CardsTeam = () => {
 
                 <div className="z-[4] w-[62px] max-md:w-[45px] h-[23px] max-md:h-[16px] bg-[#1E1B4C] absolute right-0 bottom-[7%] flex flex-col items-center filter drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] opacity-95">
                     <p className="w-max text-[#D9D9D9] text-[12px] max-md:text-[8px] font-[400] font-PalanquinDark absolute left-1/2 -translate-x-1/2">
-                        Akademik
+                        {props.Divisi}
                     </p>
                     <Image
                         src="/elements/triangle.svg"
@@ -99,6 +93,7 @@ const CardsTeam = () => {
                     />
                 </div>
             </div>
+            </Link>
         </>
     );
 };
