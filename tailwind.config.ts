30336949d204a23c33996ee6318c11d5c98ce14d
radhhh <123277320+radhhh@users.noreply.github.com>
2024-10-29 00:02:49 +0700

    feat: profile page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,6 +26,9 @@ const config: Config = {
         "custom-gradient":
           "linear-gradient(105deg, rgba(210, 212, 214, 0.70) 0.6%, rgba(166, 165, 173, 0.70) 10.44%, rgba(138, 137, 150, 0.80) 37.41%, rgba(96, 95, 110, 0.80) 61.79%, #3C3B49 75.38%)",
       },
+      screens: {
+        "xs": "500px"
+      }
     },
   },
   plugins: [],
