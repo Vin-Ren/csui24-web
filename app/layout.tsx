f6b286cd475ec0246894cdd5aa3e80be0abe2123
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2025-03-24 13:32:55 +0700

    feat: Added vercel insights & analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
+
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
+
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -82,6 +87,8 @@ export default function RootLayout({
         <Navbar />
         <main className="overflow-hidden">{children}</main>
         <Footer />
+        <SpeedInsights/>
+        <Analytics mode="production"/>
       </body>
     </html>
   );
