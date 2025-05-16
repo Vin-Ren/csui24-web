b959aacb808dc05900c231f8f3a95018d64497ae
Muhammad Fauzan <fauzannmuhh@gmail.com>
2025-05-16 13:28:05 +0700

    init

---
+++ app/menfess/page.tsx
@@ -0,0 +1,10 @@
+const MenfessPage = () => {
+  return (
+    <div className="bgGrad min-h-screen flex flex-col gap-10 max-lg:gap-6 max-sm:gap-4 text-white py-52 max-lg:py-48 max-sm:py-40 px-40 max-lg:px-20 max-md:px-10 max-sm:px-6">
+      <div></div>
+      <div className="w-full h-10 border"></div>
+      <div className="w-full h-10 border"></div>
+    </div>
+  );
+};
+export default MenfessPage;
