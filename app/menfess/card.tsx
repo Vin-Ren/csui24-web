fe152190c76350552c330050ad814e476db23801
Muhammad Fauzan <fauzannmuhh@gmail.com>
2025-05-17 01:15:43 +0700

    feat: menfess

--- app/menfess/card.tsx
+++ app/menfess/card.tsx
@@ -2,6 +2,7 @@ import { Plane, MailCheck, CalendarDays } from "lucide-react";
 import formatRelativeTime from "@/lib/formatRelativeTime";
 import Link from "next/link";
 import { briefFamsData } from "@/modules/fams-data";
+import Image from "next/image";
 
 const MenfessCard = ({
   menfess,
@@ -19,12 +20,27 @@ const MenfessCard = ({
 
   return (
     <div className="w-full h-96 rounded-xl bg-[#03045e] flex flex-col  bg-opacity-30 border border-[#717174] overflow-hidden">
-      <div className="h-fit p-6 flex flex-col gap-2">
+      <div className="h-fit p-6 max-sm:p-3 flex flex-col gap-2">
         <div className="w-full flex flex-col max-sm:gap-2 gap-4 text-white">
           <div className="w-full flex gap-3 items-center">
-            <div className="flex justify-center items-center p-2 rounded-full border border-[#717174] ">
-              <Plane size={14} />
-            </div>
+            {fromIsFam ? (
+              <Image
+                src={
+                  "/" +
+                    briefFamsData.find(
+                      (fam) => fam.id === from.replace("fams/", "")
+                    )?.["image-filename"] || ""
+                }
+                alt="profile"
+                width={30}
+                height={30}
+                className="rounded-full"
+              />
+            ) : (
+              <div className="flex justify-center items-center p-2 rounded-full border border-[#717174] ">
+                <Plane size={14} />
+              </div>
+            )}
             <div className="w-full flex flex-col">
               <p className="text-xs">From</p>
               {fromIsFam ? (
@@ -48,14 +64,44 @@ const MenfessCard = ({
             </div>
           </div>
           <div className="w-full flex gap-3 items-center">
-            <div className="flex justify-center items-center p-2 rounded-full border border-[#717174] ">
-              <MailCheck size={14} />
-            </div>
+            {toIsFam ? (
+              <Image
+                src={
+                  "/" +
+                    briefFamsData.find(
+                      (fam) => fam.id === to.replace("fams/", "")
+                    )?.["image-filename"] || ""
+                }
+                alt="profile"
+                width={30}
+                height={30}
+                className="rounded-full"
+              />
+            ) : (
+              <div className="flex justify-center items-center p-2 rounded-full border border-[#717174] ">
+                <MailCheck size={14} />
+              </div>
+            )}
             <div className="w-full flex flex-col">
               <p className="text-xs">To</p>
-              <p className="text-lg font-bold truncate whitespace-nowrap overflow-hidden max-w-full">
-                {to}
-              </p>
+              {toIsFam ? (
+                <Link
+                  href={"/" + to}
+                  className="hover:opacity-80 cursor-pointer"
+                >
+                  <p className="text-lg font-bold truncate whitespace-nowrap overflow-hidden max-w-full">
+                    {
+                      briefFamsData.find(
+                        (fam) => fam.id === to.replace("fams/", "")
+                      )?.["displayed-name"]
+                    }
+                  </p>
+                </Link>
+              ) : (
+                <p className="text-lg font-bold truncate whitespace-nowrap overflow-hidden max-w-full">
+                  {to}
+                </p>
+              )}
             </div>
           </div>
         </div>
@@ -64,7 +110,7 @@ const MenfessCard = ({
       <div className="w-full h-full text-white font-sans flex items-center justify-center px-6 overflow-y-auto max-h-full">
         <p className="text-center break-words w-full">{message}</p>
       </div>
-      <div className="w-full flex flex-row-reverse p-6">
+      <div className="w-full flex flex-row-reverse p-6 max-sm:p-3">
         <div className="flex gap-1 items-center">
           <div className="flex justify-center items-center p-1 rounded-full border border-[#717174] ">
             <CalendarDays size={14} />
