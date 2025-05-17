108da3de40fe14703faf504fa488769f81b988dd
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2025-05-17 22:24:34 +0700

    Revert "fix: Canvas version error"
    This reverts commit 7975573fb7ed08a3dbf28b466b0975be038d69b0.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 import { decode } from "blurhash";
-import { createCanvas } from "@napi-rs/canvas";
+import { createCanvas } from "canvas";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
