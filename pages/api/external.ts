29666c69569f4014208c069691cf80ed73f17c8f
Muhammad Fauzan <fauzannmuhh@gmail.com>
2025-05-18 19:09:49 +0700

    feat: add external for extra server

---
+++ pages/api/external.ts
@@ -0,0 +1,60 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@/lib/generated/prisma";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    const data = await prisma.menfess.findMany({
+      select: {
+        id: true,
+        to: true,
+        from: true,
+        message: true,
+        createdAt: true,
+      },
+      where: {
+        isPosted: false,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    for (const menfess of data) {
+      const { id } = menfess;
+      await prisma.menfess.update({
+        where: {
+          id,
+        },
+        data: {
+          isPosted: true,
+        },
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Menfess fetched successfully",
+      data,
+    });
+  } else if (req.method === "POST") {
+    const { id } = req.body;
+    if (!id) {
+      return res.status(400).json({
+        success: false,
+        message: "ID is required",
+        data: null,
+      });
+    }
+    await prisma.menfess.update({
+      where: {
+        id,
+      },
+      data: {
+        isPosted: true,
+      },
+    });
+  }
+}
